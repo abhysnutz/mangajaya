8aca70e759824b1607c97cf3c56294bb70b4830f
Mohamad Syafri Lamato <maps.abhy25@gmail.com>
2020-04-22 16:56:31 +0800

    fix auth file, admin file, route auth admin

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'HomeController@index')->name('admin');
+Route::get('/admin', 'Admin\AdminController@index')->name('admin');
 
